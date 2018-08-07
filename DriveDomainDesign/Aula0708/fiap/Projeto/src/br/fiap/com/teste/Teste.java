24304ea5070e0e3e9f02a25ae1f1591d102f5c6d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-07 20:38:05 -0300

    Aula de Java

--- DriveDomainDesign/Aula0708/fiap/Projeto/src/br/fiap/com/teste/Teste.java
+++ DriveDomainDesign/Aula0708/fiap/Projeto/src/br/fiap/com/teste/Teste.java
@@ -1,13 +1,22 @@
 package br.fiap.com.teste;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Teste {
-	public static void main(String[] args) {
-		//url: jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL
-		//user: RM79935
-		//pw: 300187
-		DriverManager.getConnection("jdbc:oracle:thin:@localhost", user, password)
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection conn = null;
+		conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM79935", "300187");
+		
+		try {
+			System.out.println("Conexão aberta");
+		} finally {
+			conn.close();
+			System.out.println("Conexão fechada");
+		}
 		
 	}
 }
