e02f54b448a9f3edef2b88f2cc91572a3f92f53f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-14 21:58:38 -0300

    aula de site responsivo

---
+++ ResponsiveWebDesign/Aula1509/js/script.js
@@ -0,0 +1,11 @@
+function btnMenu() {
+    var menu = document.getElementsByClassName('menu');
+    // Quebrando a chamada direta do menu
+    menu[0].style.display = "block";
+}
+
+function btnClose() {
+    var menu = document.getElementsByClassName('menu');
+    // Quebrando a chamada direta do menu
+    menu[0].style.display = "none";
+}
