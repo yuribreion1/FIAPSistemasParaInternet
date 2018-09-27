80d6ea7f812ff5e23dfd21ee53c333e9681d970e
yuribreion1 <yuribreion@gmail.com>
2018-09-27 18:55:05 -0300

    Acerto de listagem

--- DriveDomainDesign/am-bp/BeautyPathWeb/src/br/com/beautypath/web/ListaCatalogos.java
+++ DriveDomainDesign/am-bp/BeautyPathWeb/src/br/com/beautypath/web/ListaCatalogos.java
@@ -15,7 +15,7 @@ import br.com.beautypath.dao.CatalogoDAO;
 import br.com.beautypath.dao.ConnectionFactory;
 import br.com.beautypath.modelo.Catalogo;
 
-@WebServlet(urlPatterns = "/lista-clientes")
+@WebServlet(urlPatterns = "/lista-catalogo")
 public class ListaCatalogos extends HttpServlet {
 
 	/**
@@ -26,12 +26,14 @@ public class ListaCatalogos extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		Connection conexao;
 		try {
 			conexao = ConnectionFactory.controlarInstancia().getConnection("rm79935", "300187");
 			List<Catalogo> catalogos = new CatalogoDAO().getCatalogos(conexao);
 			req.setAttribute("catalogos", catalogos);
+			
+			
 			RequestDispatcher dispatcher = req.getRequestDispatcher("listaCatalogo.jsp");
 			dispatcher.forward(req, res);
 		} catch (Exception e) {
