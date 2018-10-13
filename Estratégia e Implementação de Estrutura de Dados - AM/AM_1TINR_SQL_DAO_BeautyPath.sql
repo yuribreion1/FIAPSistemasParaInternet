a250b0002c3d293e4017baa71f47ed3c1d7f6c29
Yuri Breion <yuribreion@gmail.com>
2018-10-13 13:04:09 -0300

    Removendo comentarios

--- Estratégia e Implementação de Estrutura de Dados - AM/AM_1TINR_SQL_DAO_BeautyPath.sql
+++ Estratégia e Implementação de Estrutura de Dados - AM/AM_1TINR_SQL_DAO_BeautyPath.sql
@@ -1,5 +1,3 @@
-/* Colocar todas as colunas na instrução do select */
-
 -- Agendamento
 
 insert into rm79935.agendamento(COD_AGD, DATA_AGD, HORA_AGD, COD_CLI, COD_PROF, OBS, COD_END) values (SEQ_AGENDAMENTO.NEXTVAL, ?, ?, ?, ?, ?, ? );
