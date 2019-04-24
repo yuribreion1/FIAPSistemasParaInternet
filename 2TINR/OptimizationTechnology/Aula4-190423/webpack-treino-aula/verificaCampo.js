2b85fa18fa49d198b206c4e0d8b454048394154c
yuribreion <yuribreion@br.ibm.com>
2019-04-23 22:33:18 -0300

    Melhorando formularios

--- 2TINR/OptimizationTechnology/Aula4-190423/webpack-treino-aula/verificaCampo.js
+++ 2TINR/OptimizationTechnology/Aula4-190423/webpack-treino-aula/verificaCampo.js
@@ -1,44 +1,49 @@
 var constantes = require('./constantes.js');
 
-function verificaCampo() {
-    constantes.checar.addEventListener("click", function () {
-        var adivinhado = constantes.campo.value;
+function jogo() {
+    var adivinhado = constantes.campo.value;
 
-        if (adivinhado > constantes.sorteado) {
-            UIkit.notification({
-                message: 'É menor',
-                status: 'danger',
-                pos: 'bottom-center',
-                timeout: 3000
-            });
-        } else if (adivinhado < constantes.sorteado) {
-            UIkit.notification({
-                message: 'É maior',
-                status: 'warning',
-                pos: 'bottom-center',
-                timeout: 3000
-            });
-        } else if (adivinhado == constantes.sorteado) {
-            UIkit.notification({
-                message: 'Acertou',
-                status: 'success',
-                pos: 'bottom-center',
-                timeout: 2000
-            });
-            UIkit.notification({
-                message: 'Recarregando pagina em 3 segundos para próxima jogada',
-                status: 'primary',
-                pos: 'bottom-center',
-                timeout: 1000
-            });
-            setTimeout(() => {
-                document.location.reload(true);
-            }, 3000);
-        } else {
-            console.log("Corrigir o código");
-            
-        }
-    })
+    if (adivinhado > constantes.sorteado) {
+        UIkit.notification({
+            message: 'É menor',
+            status: 'danger',
+            pos: 'bottom-center',
+            timeout: 3000
+        });
+        constantes.campo.select();
+    } else if (adivinhado < constantes.sorteado) {
+        UIkit.notification({
+            message: 'É maior',
+            status: 'warning',
+            pos: 'bottom-center',
+            timeout: 3000
+        });
+        constantes.campo.select();
+    } else if (adivinhado == constantes.sorteado) {
+        UIkit.notification({
+            message: 'Acertou',
+            status: 'success',
+            pos: 'bottom-center',
+            timeout: 2000
+        });
+        UIkit.notification({
+            message: 'Recarregando pagina em 3 segundos para próxima jogada',
+            status: 'primary',
+            pos: 'bottom-center',
+            timeout: 1000
+        });
+        setTimeout(() => {
+            document.location.reload(true);
+        }, 3000);
+    } else {
+        console.log("Corrigir o código");
+        
+    }
+}
+
+function verificaCampo() {
+    constantes.checar.addEventListener("click", jogo);
+    constantes.campo.addEventListener("change", jogo);
 }
 
 module.exports = verificaCampo;
